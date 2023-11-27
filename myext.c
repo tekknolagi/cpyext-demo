500978faa9f2764bc8742f8695ad06da024fee52
Max Bernstein <max@bernsteinbear.com>
2023-11-27 13:56:47 -0500

    Add typing metadata and call in a loop

--- myext.c
+++ myext.c
@@ -1,16 +1,40 @@
 #include "Python.h"
 
+long inc_impl(long arg) {
+  return arg+1;
+}
+
 PyObject* inc(PyObject* module, PyObject* obj) {
   (void)module;
   long obj_int = PyLong_AsLong(obj);
   if (obj_int == -1 && PyErr_Occurred()) {
     return NULL;
   }
-  return PyLong_FromLong(obj_int+1);
+  long result = inc_impl(obj_int);
+  return PyLong_FromLong(result);
 }
 
+enum ObjectType {
+  T_OBJECT,
+  T_INT,
+};
+
+struct TypedMethodMetadata {
+  enum ObjectType arg_type;
+  enum ObjectType ret_type;
+  void* underlying_func;
+  const char ml_name[100];
+};
+
+struct TypedMethodMetadata inc_sig = {
+  .arg_type = T_INT,
+  .ret_type = T_INT,
+  .underlying_func = inc_impl,
+  .ml_name = "inc",
+};
+
 static PyMethodDef myext_methods[] = {
-    {"inc", inc, METH_O, "Add one to an int"},
+    {inc_sig.ml_name, inc, METH_O, "Add one to an int"},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef myext_definition = {
--- myext_test.py
+++ myext_test.py
@@ -1,3 +1,9 @@
 import myext
 
-print(myext.inc(2))
+def main(x):
+    for i in range(10000):
+        myext.inc(x)
+
+main(2)
+print(myext.__name__)
+print(myext.inc.__name__)
